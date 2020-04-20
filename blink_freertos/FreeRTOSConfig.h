1b3b8f37619172997b70b89293ecbb7c97f6efcf
emwhbr <hakanbrolin@hotmail.com>
2020-04-20 19:44:16 +0200

    Add support for FreeRTOS aware debugger in project 'blink_freertos'.

--- blink_freertos/FreeRTOSConfig.h
+++ blink_freertos/FreeRTOSConfig.h
@@ -51,7 +51,6 @@
 #define configTICK_RATE_HZ              ( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES            ( 5 )
 #define configMAX_TASK_NAME_LEN         ( 10 )
-#define configUSE_TRACE_FACILITY        0
 #define configUSE_16_BIT_TICKS          0
 #define configIDLE_SHOULD_YIELD         1
 #define configUSE_MUTEXES               0
@@ -62,6 +61,12 @@
 #define configSUPPORT_DYNAMIC_ALLOCATION  1
 #define configMINIMAL_STACK_SIZE          ( ( unsigned short ) 128 )
 #define configTOTAL_HEAP_SIZE             ( ( size_t ) ( 10 * 1024 ) )
+
+/* Run time and task stats gathering related definitions. */
+#define configGENERATE_RUN_TIME_STATS           0
+#define configUSE_TRACE_FACILITY                1
+#define configUSE_STATS_FORMATTING_FUNCTIONS    1
+#define configRECORD_STACK_HIGH_ADDRESS         1
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES             0
